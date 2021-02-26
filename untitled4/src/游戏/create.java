816182d6473aa544c3561b94ac24fae014c022bb
Z1234556 <74126057+Z1234556@users.noreply.github.com>
2021-02-26 12:51:31 +0800

    Add files via upload

--- untitled4/src/游戏/create.java
+++ untitled4/src/游戏/create.java
@@ -1,52 +1,47 @@
 package 游戏;
 
-import java.security.PublicKey;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class create {
+    int lpr;
+    int apr;
+    int dpr;
+    String name;
+
     public String name(){
         Scanner sn =new Scanner(System.in);
-        String n =sn.nextLine();
-        return n;
+        AtomicReference<String> n = new AtomicReference<>(sn.nextLine());
+        return n.get();
     }
+
     public int setNumber(){
         Scanner sc =new Scanner(System.in);
-        int number= sc.nextInt();
-        return number;
+        AtomicInteger number= new AtomicInteger(sc.nextInt());
+        return number.get();
+    }
+
+    void inter(String d){
+        System.out.println("请为您创建的"+d+"命名");
+        name = name();
+        System.out.println("接下来，请为您创建的"+d+"设定三维属性（限定范围1·999）");
+        System.out.println("请为您创建的人物设定   生命力：");
+        lpr = setNumber();
+        System.out.println("请为您创建的人物设定   攻击力：");
+        apr = setNumber();
+        System.out.println("请为您创建的人物设定   防御力：");
+        dpr = setNumber();
     }
 
 }
 class Person extends create{
-    int lpr;
-    int apr;
-    int dpr;
-    String name;
-      void inter(){
-        System.out.println("请为您创建的人物命名");
-        name = super.name();
-        System.out.println("接下来，请为您创建的人物设定三维属性（限定范围1·999）");
-        System.out.println("请为您创建的人物设定   生命力：");
-        lpr = super.setNumber();
-        System.out.println("请为您创建的人物设定   攻击力：");
-        apr = super.setNumber();
-        System.out.println("请为您创建的人物设定   防御力：");
-        dpr = super.setNumber();
-    }
+   void inter(){
+       super.inter("人物");
+   }
 }
 class Monster extends create{
-    int lpr;
-    int apr;
-    int dpr;
-    String name;
-    void inter(){
-        System.out.println("请为您创建的怪物命名");
-        name = super.name();
-        System.out.println("接下来，请为您创建的怪物设定三维属性（限定范围1·999）");
-        System.out.println("请为您创建的怪物设定   生命力：");
-        lpr = super.setNumber();
-        System.out.println("请为您创建的怪物设定   攻击力：");
-        apr = super.setNumber();
-        System.out.println("请为您创建的怪物设定   防御力：");
-        dpr = super.setNumber();
-    }
+     void inter(){
+         super.inter("怪物");
+     }
 }
--- untitled4/src/游戏/game.java
+++ untitled4/src/游戏/game.java
@@ -1,7 +1,5 @@
 package 游戏;
 
-import com.sun.jdi.event.ClassPrepareEvent;
-
 public class game {
     public static void main(String[] args){
          fight c=new fight();
